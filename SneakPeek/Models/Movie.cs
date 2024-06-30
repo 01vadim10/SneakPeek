c83cd0b50b26d93d584a10b7b112150f86193413
prokharovaky <144281695+prokharovaky@users.noreply.github.com>
2024-06-30 14:31:11 +0300

    Add class Movie

---
+++ SneakPeek/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿namespace SneakPeek.Models
+{
+    public class Movie
+    {
+        public string title { get; set; }
+        public string wait { get; set; }
+        public string description { get; set; }
+        public DateTime release_date { get; set; }
+        public string genre { get; set; }
+        public string director { get; set; }
+        public double rating { get; set; }
+    }
+}
