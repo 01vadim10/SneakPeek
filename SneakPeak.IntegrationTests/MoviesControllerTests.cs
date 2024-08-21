1db91f28f7cd889e9d7e0cdfbe557eb078f1f820
prokharovaky <144281695+prokharovaky@users.noreply.github.com>
2024-08-21 14:50:48 +0300

    Make all file scoped

--- SneakPeak.IntegrationTests/MoviesControllerTests.cs
+++ SneakPeak.IntegrationTests/MoviesControllerTests.cs
@@ -3,30 +3,29 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using FluentAssertions;
 using System.Net;
 
-namespace SneakPeak.Tests
+namespace SneakPeak.Tests;
+
+public class MoviesControllerTests : IClassFixture<WebApplicationFactory<Program>>
 {
-    public class MoviesControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public MoviesControllerTests(WebApplicationFactory<Program> factory)
     {
-        private readonly WebApplicationFactory<Program> _factory;
+        _factory = factory;
+    }
 
-        public MoviesControllerTests(WebApplicationFactory<Program> factory)
-        {
-            _factory = factory;
-        }
+    [Fact]
+    public async Task Get_MoviesEndpointsReturnSuccessAndCorrectContentType()
+    {
+        //Arrange
+        const string url = "/movies";
+        var client = _factory.CreateClient();
 
-        [Fact]
-        public async Task Get_MoviesEndpointsReturnSuccessAndCorrectContentType()
-        {
-            //Arrange
-            const string url = "/movies";
-            var client = _factory.CreateClient();
+       //Act
+        var response = await client.GetAsync(url);
 
-           //Act
-            var response = await client.GetAsync(url);
-
-            //Assert
-            response.StatusCode.Should().Be(HttpStatusCode.OK);
-            response.Content.Headers.ContentType.ToString().Should().Be("application/json; charset=utf-8");
-        }
+        //Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        response.Content.Headers.ContentType.ToString().Should().Be("application/json; charset=utf-8");
     }
 }
--- SneakPeek/Models/Movie.cs
+++ SneakPeek/Models/Movie.cs
@@ -1,13 +1,12 @@
-﻿namespace SneakPeek.Models
+﻿namespace SneakPeek.Models;
+
+public class Movie
 {
-    public class Movie
-    {
-        public string title { get; set; }
-        public string wait { get; set; }
-        public string description { get; set; }
-        public string release_date { get; set; }
-        public string genre { get; set; }
-        public List<string> director { get; set; }
-        public double rating { get; set; }
-    }
+    public string title { get; set; }
+    public string wait { get; set; }
+    public string description { get; set; }
+    public string release_date { get; set; }
+    public string genre { get; set; }
+    public List<string> director { get; set; }
+    public double rating { get; set; }
 }
