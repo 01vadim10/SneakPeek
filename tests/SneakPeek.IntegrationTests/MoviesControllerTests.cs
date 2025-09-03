4bd285fe1267b5b22574ee341b6d12c779f633cd
lo1d1 <kovalchukyura0405@gmail.com>
2025-09-03 21:15:19 +0200

    Change old Integration test for new MovieController

--- tests/SneakPeek.IntegrationTests/MoviesControllerTests.cs
+++ tests/SneakPeek.IntegrationTests/MoviesControllerTests.cs
@@ -1,38 +1,60 @@
 using Microsoft.AspNetCore.Mvc.Testing;
-using FluentAssertions;
+using Moq;
 using SneakPeek.Models;
+using FluentAssertions;
+using Domain.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
 using System.Net;
 using System.Text.Json;
 
-namespace SneakPeak.IntegrationTests;
-
-public class MoviesControllerTests : IClassFixture<WebApplicationFactory<Program>>
+public class MovieControllerTest : IClassFixture<WebApplicationFactory<Program>>
 {
     private readonly WebApplicationFactory<Program> _factory;
 
-    public MoviesControllerTests(WebApplicationFactory<Program> factory)
-    {
+    public MovieControllerTest(WebApplicationFactory<Program> factory)
+    { 
         _factory = factory;
     }
 
     [Fact]
-    public async Task Get_MoviesEndpointsReturnSuccessAndCorrectContentType()
-    {
-        // Arrange
-        const string url = "/movies";
-        var client = _factory.CreateClient();
+    public async Task Get_WhenMoviesExist_ReturnsSuccessAndListOfMovies()
+    { 
+        var testMovies = new List<Movie>
+        { 
+            new Movie { Id = 1, Title = "Title 1"},
+            new Movie { Id = 2, Title = "Title 2"}
+        };
 
-        //Act
+        var repoMock = new Mock<IMoviesRepository>();
+        repoMock.Setup(r => r.GetAllMoviesAsync()).ReturnsAsync(testMovies);
+
+        var client = _factory.WithWebHostBuilder(
+            builder =>
+            {
+                builder.ConfigureServices(
+                    services =>
+                    {
+                        var descriptor = services.SingleOrDefault(s => s.ServiceType == typeof(IMoviesRepository));
+                        if (descriptor != null)
+                        {
+                            services.Remove(descriptor);
+                        }
+                        services.AddScoped<IMoviesRepository>(_=>repoMock.Object);
+                    });
+            }).CreateClient();
+
+        const string url = "/movies";
         var response = await client.GetAsync(url);
 
-        // Assert
         response.StatusCode.Should().Be(HttpStatusCode.OK);
-        response.Content.Headers.ContentType.ToString().Should().Be("text/json; charset=utf-8");
 
         var json = await response.Content.ReadAsStringAsync();
-        var movies = JsonSerializer.Deserialize<List<Movie>>(json);
 
-        movies.Should().NotBeNull();
-        movies?.Count.Should().BeGreaterThan(0);
+        var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true};
+        var moviesFromApi = JsonSerializer.Deserialize<List<Movie>>(json, options);
+
+        moviesFromApi.Should().NotBeNull();
+        moviesFromApi.Should().HaveCount(2);
+        moviesFromApi.Should().BeEquivalentTo(testMovies);
     }
 }
